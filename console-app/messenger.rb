720b69c14e2405a230a28a64956adc7f5a21eea7
Azamat Jumabekov <azamat.jumabekov@gmail.com>
2017-03-15 15:01:42 +0600

    Refactoring consolea-app

--- console-app/messenger.rb
+++ console-app/messenger.rb
@@ -2,30 +2,24 @@
 class Sender
   require 'net/http'
   require 'readline'
-  require 'pry'
 
-  templates = Dir[ '../templates/*' ].select{ |f| File.file? f }.map{ |f| File.basename f }
-  LIST = %w(template to params).push(*templates).sort
-
-  def initialize
-    @uri = URI.parse("http://192.168.1.103:9393/generate")
-  end
-  def sender_params
+  def self.send(line)
+    uri = URI.parse("http://192.168.40.65:3000/generate")
     header = {'Content-Type' => 'application/json'}
-    Net::HTTP::Post.new(@uri.request_uri, header)
-  end
-
-  def sender(line)
-    request = sender_params
-    http = Net::HTTP.new(@uri.host, @uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = line
+    http = Net::HTTP.new(uri.host, uri.port)
     response = http.request(request)
-    show_wait_cursor(1)
+    App.show_wait_cursor(1)
     puts response.body.white
-  end
+  end      
+end
 
+class App
+  TEMPLATES = Dir[ '../templates/*' ].select{ |f| File.file? f }.map{ |f| File.basename f }
+  LIST = %w(template to params).push(*TEMPLATES).sort
 
-  def show_wait_cursor(seconds,fps=10)
+  def self.show_wait_cursor(seconds,fps=10)
     chars = %w[| / - \\]
     delay = 1.0/fps
     (seconds*fps).round.times{ |i|
@@ -35,23 +29,23 @@ class Sender
     }
   end
 
-  def run
+  def self.run
     complete = proc { |s| LIST.grep(/^#{Regexp.escape(s)}/) }
     Readline.completion_append_character = " "
     Readline.completion_proc = complete
     loop do
       puts "> Enter params".cyan
       while line = Readline.readline('> '.cyan, true)
-        sender(line)
+          Sender.send(line)  
       end
-    end    
-  end    
+    end
+  end
 end
+
 class String
   def magenta;        "\e[35m#{self}\e[0m" end
   def cyan;           "\e[36m#{self}\e[0m" end
   def white;          "\e[37m#{self}\e[1m" end
 end
 
-app = Sender.new
-app.run
+App.run
