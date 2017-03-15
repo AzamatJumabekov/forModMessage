be57fc8758355922e246a49f6b198af4fbe06bd5
Azamat Jumabekov <azamat.jumabekov@gmail.com>
2017-03-15 10:10:30 +0600

    #13 Modified console app. Added auto completion, colors and interactive wait cursor. Just for fun

--- console-app/messenger.rb
+++ console-app/messenger.rb
@@ -1,14 +1,55 @@
 #!/usr/bin/env ruby
-require 'net/http'
+class Sender
+  require 'net/http'
+  require 'readline'
+  require 'pry'
 
-uri = URI.parse("http://192.168.40.67:3000/generate")
-header = {'Content-Type' => 'application/json'}
+  templates = Dir[ '../templates/*' ].select{ |f| File.file? f }.map{ |f| File.basename f }
+  LIST = %w(template to params).push(*templates).sort
 
-# Create the HTTP objects
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri, header)
-request.body = ARGV[0]
+  def sender_params
+    uri = URI.parse("http://192.168.1.103:9393/generate")
+    header = {'Content-Type' => 'application/json'}
+    Net::HTTP::Post.new(@uri.request_uri, header)
+  end
 
-# Send the request
-response = http.request(request)
-puts response.body
+  def sender(line)
+    request = sender_params
+    http = Net::HTTP.new(@uri.host, @uri.port)
+    request.body = line
+    response = http.request(request)
+    show_wait_cursor(1)
+    puts response.body.white
+  end
+
+
+  def show_wait_cursor(seconds,fps=10)
+    chars = %w[| / - \\]
+    delay = 1.0/fps
+    (seconds*fps).round.times{ |i|
+      print chars[i % chars.length].magenta
+      sleep delay
+      print "\b"
+    }
+  end
+
+  def run
+    complete = proc { |s| LIST.grep(/^#{Regexp.escape(s)}/) }
+    Readline.completion_append_character = " "
+    Readline.completion_proc = complete
+    loop do
+      puts "> Enter params".cyan
+      while line = Readline.readline('> '.cyan, true)
+        sender(line)
+      end
+    end    
+  end    
+end
+class String
+  def magenta;        "\e[35m#{self}\e[0m" end
+  def cyan;           "\e[36m#{self}\e[0m" end
+  def white;          "\e[37m#{self}\e[1m" end
+end
+
+app = Sender.new
+app.run
