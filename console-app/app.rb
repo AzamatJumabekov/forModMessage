b923e24b181262daaa0947d161958775fed6cd50
Azamat Jumabekov <azamat.jumabekov@gmail.com>
2017-03-23 15:10:13 +0600

    Added rubocop to project

--- console-app/app.rb
+++ console-app/app.rb
@@ -2,22 +2,27 @@
 require_relative 'sender'
 
 class App
-  TEMPLATES = Dir[ '../templates/*' ].select{ |f| File.file? f }.map{ |f| File.basename f }
+  TEMPLATES = Dir['../templates/*'].select { |f| File.file? f }.map { |f| File.basename f }
   LIST = %w(template to params).push(*TEMPLATES).sort
 
   def self.run
     complete = proc { |s| LIST.grep(/^#{Regexp.escape(s)}/) }
-    Readline.completion_append_character = " "
+    Readline.completion_append_character = ''
     Readline.completion_proc = complete
-    loop do
-      puts "> Enter params".cyan
-      while line = Readline.readline('> '.cyan, true)
+      loop do
+        puts '> Enter params'.cyan
+        line = Readline.readline('> '.cyan, true)
+        case line
+        when 'exit'
+          break
+        when 'help'
+          puts 'Parameters must be passed as ruby Hash'
+        else
           request = Sender.new
           response = request.send(line)
           puts response.body
+        end
       end
-    end
   end
 end
-
 App.run
--- console-app/sender.rb
+++ console-app/sender.rb
@@ -3,20 +3,19 @@ require 'net/http'
 require 'readline'
 
 class Sender
-
-  def show_wait_cursor(seconds,fps=10)
-    chars = %w[| / - \\]
-    delay = 1.0/fps
-    (seconds*fps).round.times{ |i|
+  def show_wait_cursor(seconds, fps = 10)
+    chars = %w(| / - \\)
+    delay = 1.0 / fps
+    (seconds * fps).round.times do |i|
       print chars[i % chars.length].magenta
       sleep delay
       print "\b"
-    }
+    end
   end
 
   def send(line)
-    uri = URI.parse("http://web-app:3000/generate")
-    header = {'Content-Type' => 'application/json'}
+    uri = URI.parse('http://web-app:3000/generate')
+    header = { 'Content-Type' => 'application/json' }
     request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = line
     http = Net::HTTP.new(uri.host, uri.port)
